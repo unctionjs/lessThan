ce58e8413224bbf962ae770078343df30915077b
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-27 18:51:05 -0700

    Going back to how other javascript packages do it, makes it eaiser on everyone :/

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type, no-magic-numbers */
 import {test} from "tap"
 
-import lessThan from "./source.js"
+import lessThan from "./"
 
 test(({same, end}) => {
   same(
